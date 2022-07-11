827b5767e4d15dd50c3283d24e27d62cd7b39b5a
Ryan Sims <ryanjsims@email.arizona.edu>
2022-07-11 03:56:02 -0700

    Let test_dme open any arbitrary file

--- test_dme.py
+++ test_dme.py
@@ -1,6 +1,12 @@
 import os
 import struct
+from argparse import ArgumentParser
 from dme_loader import DME, D3DXParamClass, D3DXParamType, jenkins
 
-with open("dme/assets_x64_23/1598760830853998527.dme", "rb") as f:
+parser = ArgumentParser(description="DME Tester for cmd line inspection of DME files")
+parser.add_argument("file", help="The file to load")
+args = parser.parse_args()
+
+with open(args.file, "rb") as f:
     dme = DME.load(f)
+
