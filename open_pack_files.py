05f2ce5bcdf717d03d5fdb99158fff6037134ecf
Ryan Sims <ryanjsims@email.arizona.edu>
2022-11-03 00:34:33 -0700

    Add some additional conveniences to playground files

--- open_pack_files.py
+++ open_pack_files.py
@@ -4,6 +4,7 @@ from glob import glob
 from multiprocessing import Pool
 from pathlib import Path
 from export_manager import ExportManager
+from argparse import ArgumentParser
 
 logger = logging.getLogger("Open Pack Files")
 
@@ -27,8 +28,18 @@ manager = None
 def main():
     global manager, paths, handler
     logging.basicConfig(level=logging.INFO, handlers=[handler])
+
+    parser = ArgumentParser(description="Opens pack files for interactive use")
+    parser.add_argument("-n", "--namelist", default=None, type=str, help="Namelist file to load for additional file names")
+    args = parser.parse_args()
+
+    namelist = None
+    if args.namelist is not None:
+        with open(args.namelist) as f:
+            namelist = f.read().split("\n")
+
     with Pool(8) as pool:
-        manager = ExportManager(paths, p=pool)
+        manager = ExportManager(paths, p=pool, namelist=namelist)
         print("Loading packs...")
         manager.loaded.wait()
         print("Loaded.")
--- test_dme.py
+++ test_dme.py
@@ -2,6 +2,20 @@ import os
 import struct
 from argparse import ArgumentParser
 from dme_loader import DME, D3DXParamClass, D3DXParamType, jenkins
+from numpy import matrix
+from english_words import english_words_lower_alpha_set as words
+
+import logging
+
+logger = logging.getLogger("DME testing")
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(
+    fmt="[%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s] %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+))
+
+logging.basicConfig(level=logging.INFO, handlers=[handler])
 
 parser = ArgumentParser(description="DME Tester for cmd line inspection of DME files")
 parser.add_argument("file", help="The file to load")
@@ -10,3 +24,14 @@ args = parser.parse_args()
 with open(args.file, "rb") as f:
     dme = DME.load(f)
 
+for bone in dme.bones:
+    print(bone)
+    print(*list(map(lambda x: -x, (bone.inverse_bind_pose * [[0], [0], [0], [1]]).flatten().tolist()[0])))
+    print()
+
+def bone_name(dme: DME, name: str):
+    namehash = jenkins.oaat(name.encode("utf-8"))
+    for bone in dme.bones:
+        if bone.name == '' and bone.namehash == namehash:
+            print(f'{namehash}: "{name}",')
+            bone.name = name
